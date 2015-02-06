18e8ce906aa778c2df927ae1c1636f0c412195b7
itsmikej <jiangyang33@gmail.com>
2015-02-06 15:46:44 +0800

    添加Filter组件和安全组件

--- Moonrise/Component/Filter.php
+++ Moonrise/Component/Filter.php
@@ -7,10 +7,51 @@
 
 namespace Moonrise\Component;
 
+use Moonrise\Core\Loader;
+use Moonrise\Core\Registry;
+
 class Filter
 {
-    public function filter($value, $var_type, $options=array())
+    public function __construct()
     {
-        return func_get_args();
+    }
+
+    public function filter(&$value, $var_type, $options=array('max'=>null, 'min'=>null, 'length'=>null, 'regex'=>null, 'default'=>null, 'default_key'=>null, 'pre_func'=>null, 'array'=>array(), 'enum_key'=>false, 'strict'=>false, 'xss_clean'=>false))
+    {
+        if (!isset($value)) {
+            if (isset($options['default'])) {
+                return $options['default'];
+            }
+            return null;
+        }
+
+        if (isset($options['pre_func']) && function_exists($options['pre_func'])) {
+            $value = call_user_func($options['pre_func'], $value);
+        }
+
+        if (isset($options['xss_clean']) && $options['xss_clean']) {
+            $securityObj = Registry::getComponent('security');
+
+        }
+
+        switch ($var_type) {
+            case MR_STR:
+                $this->filterStr($value, $options);
+                break;
+            case MR_INT:
+                break;
+
+            case MR_DEFAULT:
+            default:
+                $this->filterStr($value, $options);
+                break;
+        }
+
+        return $value;
+    }
+
+    protected function filterStr(&$value, $options)
+    {
+
     }
 }
--- index.php
+++ index.php
@@ -5,24 +5,13 @@
  * @author itsmikej
  */
 
-# todo 整合入口所有定义
-
 define('MR_TIMESTAMP', time());
 define('MR_DATETIME', date('Y-m-d H:i:s', MR_TIMESTAMP));
 
 define('BASE_DIR', __DIR__);
 define('APPDATA_DIR', BASE_DIR . '/Appdata');
 
-$loader = require(BASE_DIR . '/vendor/autoload.php');
-require(BASE_DIR . '/Moonrise/Core/Loader.php');
-require(BASE_DIR . '/Moonrise/Functions/Common.php');
-
-# todo 测试单元
-//$loader->add('Moonrise\\', __DIR__);
-//$loader->add('Control\\', __DIR__);
-
-spl_autoload_register("\\Moonrise\\Core\\Loader::AutoLoad");
-
+# 运行方式
 if (PHP_SAPI == 'cli' || defined('STDIN')) {
     define('MR_INTERFACE', 'cli');
 } else {
@@ -30,6 +19,13 @@ if (PHP_SAPI == 'cli' || defined('STDIN')) {
 }
 
 define('MR_DEBUG', true);
+
+require(BASE_DIR . '/Moonrise/Constants.php');
+require(BASE_DIR . '/vendor/autoload.php');
+require(BASE_DIR . '/Moonrise/Core/Loader.php');
+require(BASE_DIR . '/Moonrise/Functions/Common.php');
+
+spl_autoload_register("\\Moonrise\\Core\\Loader::AutoLoad");
 
 $app = new Moonrise\Start();
 

6bfb9e7e3f1925231b4a8043fe9acf9453b01885
itsmikej <jiangyang33@gmail.com>
2015-03-01 19:37:05 +0800

    添加 Active Record类

---
+++ Moonrise/Database/ActiveRecord.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Active Record类
+ *
+ * @author itsmikej
+ */
+
+namespace Moonrise\Database;
+
+class ActiveRecord
+{
+    public function __construct()
+    {
+
+    }
+
+    public function init(DbDriver $dbDriver)
+    {
+
+    }
+
+    public function get($table='', $limit='', $offset='')
+    {
+
+    }
+
+    public function from()
+    {
+
+    }
+
+    public function limit()
+    {
+
+    }
+}
